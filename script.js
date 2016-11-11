d3a815d233dbd6f7fc9e0442779e0aa625ba5486
Michael Friedman <contact@michaeldavidfriedman.com>
2016-11-11 10:46:30 -0800

    Added solution and did some refactoring.

--- script.js
+++ script.js
@@ -12,13 +12,23 @@
 (function () {
   'use strict'
   const setOrange = () => {
+    if (slowLight.style.backgroundColor === 'orange') {
+      
+    }
     slowLight.style = 'background-color: orange'
   }
   const setBlack = () => {
     slowLight.style = 'background-color: black'
+    stopLight.style = 'background-color: black'
+    goLight.style = 'background-color: black'
   }
   const setRed = () => {
     stopLight.style = 'background-color: red'
+    slowLight.style = 'background-color: black'
+    goLight.style = 'background-color: black'
+  }
+  const setGreen = () => {
+    goLight.style = 'background-color: green'
   }
   const stopButton = document.getElementById('stopButton')
   const stopLight = document.getElementById('stopLight')
@@ -27,63 +37,36 @@
   const goButton = document.getElementById('goButton')
   const goLight = document.getElementById('goLight')
   const buttons = document.querySelectorAll('h1')
-  let redCounter = 0
-  let slowCounter = 0
-  let goCounter = 0
   stopButton.addEventListener('click', () => {
-    redCounter += 1
-    if (redCounter % 2 === 1) {
-      stopLight.style = 'background-color: red'
-      goLight.style = 'background-color: black'
-      slowLight.style = 'background-color: black'
+    if (stopLight.style.backgroundColor === 'red') {
+      setBlack()
     } else {
-      stopLight.style = 'background-color: black'
+      setRed()
     }
   })
   slowButton.addEventListener('click', () => {
-    slowCounter += 1
-    if (slowCounter % 2 === 1) {
-      slowLight.style = 'background-color: orange'
-      stopLight.style = 'background-color: black'
-      goLight.style = 'background-color: black'
-    } else {
-      slowLight.style = 'background-color: black'
-    }
+
   })
   goButton.addEventListener('click', () => {
-    goCounter += 1
-    if (goCounter % 2 === 1) {
-      goLight.style = 'background-color: green'
-      stopLight.style = 'background-color: black'
-      slowLight.style = 'background-color: black'
-    } else {
-      goLight.style = 'background-color: black'
+    if (goLight.style.backgroundColor === 'green') {
+      setBlack()
       setOrange()
-      setTimeout(setBlack, 5000)
-      setTimeout(setRed, 5000)
+      setTimeout(setRed, 3000)
+    } else {
+    setGreen()
+    slowLight.style = 'background-color: black'
+    stopLight.style = 'background-color: black'
     }
-  })
-  stopButton.addEventListener('mouseover', () => {
-    console.log(`Entered stop button.`)
-  })
-  slowButton.addEventListener('mouseover', () => {
-    console.log(`Entered slow button.`)
-  })
-  goButton.addEventListener('mouseover', () => {
-    console.log(`Entered go button.`)
-  })
-  stopButton.addEventListener('mouseout', () => {
-    console.log(`Left stop button.`)
-  })
-  slowButton.addEventListener('mouseout', () => {
-    console.log(`Left slow button.`)
-  })
-  goButton.addEventListener('mouseout', () => {
-    console.log(`Left go button.`)
   })
   for (const button of buttons) {
     button.addEventListener('click', (e) => {
       console.log(`You clicked ${button.textContent}`)
+    })
+    button.addEventListener('mouseenter', (e) => {
+      console.log(`You entered ${button.textContent}`)
+    })
+    button.addEventListener('mouseleave', (e) => {
+      console.log(`You left ${button.textContent}`)
     })
   }
 })()
